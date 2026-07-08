d0d9560f0abbe1ee7eaca097fe66a41e086dbae5
saurabh <damalesaurabh34@gmail.com>
2026-07-08 18:53:31 +0530

    ios-development page added

---
+++ ios-development.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = 'iOS App Development || Technofra Website';
+$bodyClass = 'hero-video-page tf-ios-page';
+include __DIR__ . '/header.php';
+?>
+
+<?php include __DIR__ . '/ios-development-sections.php'; ?>
+
+<?php include __DIR__ . '/footer.php'; ?>
